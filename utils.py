97e6e8718eb018ac289094f6b1c7ecd2b52eeb44
alinasser2 <44727847+alinasser2@users.noreply.github.com>
2023-03-17 15:32:43 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,49 +5,35 @@ from django.conf import settings
 
 
 class payment_gateway():
-
-    _api_key = "ZXlKaGJHY2lPaUpJVXpVeE1pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SnVZVzFsSWpvaWFXNXBkR2xoYkNJc0ltTnNZWE56SWpvaVRXVnlZMmhoYm5RaUxDSndjbTltYVd4bFgzQnJJam8zTVRrek5qZDkubVZaM1pmbVZDdjdnSm1RdTlfbWVhWndReHVEU0ZnWEJ4NDZYZzNzRG5zSk4tcjJBX3E3SjdSeXFCajdwUUlYaGE0RUNDYnY1RnJDaERzaFJJbHpFTlE="#settings.PAYMOB_API_KEY
+    _api_key = settings.PAYMOB_API_KEY
     _ENDPOINT: str = "https://accept.paymob.com/api"
     _TOKEN_URL: str = f"{_ENDPOINT}/auth/tokens"
     _ORDER_URL: str = f"{_ENDPOINT}/ecommerce/orders"
     _PAYMENT_KEY_URL: str = f"{_ENDPOINT}/acceptance/payment_keys"
-    _IFRAME_ID: int =  741795 # settings.PAYMOB_IFRAME_ID
-    _INTEGRATION_ID: int = 3530084# settings.PAYMOB_INTEGRATION_ID
+    _IFRAME_ID: int = settings.PAYMOB_IFRAME_ID
+    _INTEGRATION_ID: int = settings.PAYMOB_INTEGRATION_ID
     _PAY_URL: str = f"https://accept.paymob.com/api/acceptance/iframes/{_IFRAME_ID}"
 
-    # def __init__(self, total_price ,request):
-    def __init__(self, total_price, country):
+    def __init__(self, total_price, request , country , items):
         self.total_price = total_price
         self.country = country
-        # self.billing_data = {
-        #         "email": request.user.email or "mail@example.com",
-        #         "phone_number": request.user.phone,
-        #         "country": "EG",
-        #         "city": request.user.customer_profile.city,
-        #         "apartment": request.user.customer_profile.appartment,
-        #         "floor": request.user.customer_profile.floor,
-        #         "first_name": request.user.firstname,
-        #         "last_name": request.user.lastname,
-        #         "street": request.user.customer_profile.address,
-        #         "building": request.user.customer_profile.appartment,
-        #         "postal_code": request.user.customer_profile.postal_code,
-        #         "state": request.user.customer_profile.city,
-        #     }
         self.billing_data = {
-        "apartment": "032", 
-        "email": "cladett1e09@exa.com", 
-        "floor": "4", 
-        "first_name": "Csa321lifford", 
-        "street": "Ethan dLnd", 
-        "building": "80d2", 
-        "phone_number": "+826(8)9135210487", 
-        "shipping_method": "PKG", 
-        "postal_code": "01898", 
-        "city": "Jaskolskiburgh", 
-        "country": "CR", 
-        "last_name": "Nicolas", 
-        "state": "Utah"
-        }
+                "email": request.user.email ,
+                "phone_number": request.user.phone,
+                "country": "EGP",
+                "city": request.user.customer_profile.city,
+                "apartment": request.user.customer_profile.appartment,
+                "floor": request.user.customer_profile.floor,
+                "first_name": request.user.firstname,
+                "last_name": request.user.lastname,
+                "street": request.user.customer_profile.address,
+                "building": request.user.customer_profile.appartment,
+                "postal_code": request.user.customer_profile.postal_code,
+                "state": request.user.customer_profile.city,
+            }
+        self.items = items
+        
+        
 
 
     def authentication_request(self) -> str:
@@ -57,17 +43,18 @@ class payment_gateway():
         res = res.json()
         token = res['token']
         return token
+    
 
-    def order_registration(self, token : str, items : list) -> str:
+    def order_registration(self, token : str, items : list = [] , shipping_data : dict = {} , shipping_details : dict = {}) -> str:
         
         data = {
         "auth_token":  token,
         "delivery_needed": "false",
-        "amount_cents": self.total_price,
+        "amount_cents": str(self.total_price),
         "currency": "EGP",
         "items": items,
-        # "shipping_data": shipping_data,
-        # "shipping_details" : shipping_details
+        "shipping_data": shipping_data,
+        "shipping_details" : shipping_details
         }
         res = requests.post(self._ORDER_URL,json = data)
         id = res.json()['id']
@@ -83,6 +70,7 @@ class payment_gateway():
         "billing_data": self.billing_data, 
         "currency": "EGP",
         "integration_id": self._INTEGRATION_ID,
+        "lock_order_when_paid": "false"
         })
         token = res.json()['token']
         return token
@@ -91,18 +79,7 @@ class payment_gateway():
     def pay(self):
         if self.country.strip().lower() in ["egypt","egy"]:
             token = self.authentication_request()
-            order_id = self.order_registration(token , items=[{
-                "name": "ASC1515",
-                "amount_cents": "500000",
-                "description": "Smart Watch",
-                "quantity": "1"
-            },
-            { 
-                "name": "ERT6565",
-                "amount_cents": "200000",
-                "description": "Power Bank",
-                "quantity": "1"
-            }])
+            order_id = self.order_registration(token , items = self.items)
             payment_token = self.payment_key_request(id = order_id, token = token)
             return f"{self._PAY_URL}?payment_token={payment_token}"
         else:
